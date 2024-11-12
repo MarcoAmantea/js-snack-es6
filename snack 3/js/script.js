17c2ded4c359ecba0e587e3a5eeccf2ca823ce58
MarcoAmantea <marcoamantea18@gmail.com>
2024-11-12 17:25:27 +0100

    feat: snack3

---
+++ snack 3/js/script.js
@@ -0,0 +1,24 @@
+//Creiamo la funzione che accetta un array,una variabile a e una variabile b
+function includedNumbers(a, b, array){
+    const newArray = [];
+
+    //ciclo for
+    for (let i = 0; i < array.length; i++) {
+        const curArray = array[i];
+        if(curArray >= a && curArray <= b){
+            newArray.push(curArray);
+        }       
+    }
+    //ci deve ritornare il nuovo array
+    return newArray
+}
+
+const numArray = [12,34,14,54,32,43,15,98,22,34,11]
+const a = 5
+const b = 20
+
+console.log(includedNumbers(a,b,numArray));
+
+    
+
+
