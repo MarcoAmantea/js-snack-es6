ea6de407a9ddab2ca80211714ce0d80b8505f79f
MarcoAmantea <marcoamantea18@gmail.com>
2024-11-13 10:09:52 +0100

    refactor: snack3

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -5,7 +5,7 @@ function includedNumbers(a, b, array){
     //ciclo for
     for (let i = 0; i < array.length; i++) {
         const curArray = array[i];
-        if(curArray >= a && curArray <= b){
+        if(i >= a && i <= b){
             newArray.push(curArray);
         }       
     }
@@ -15,7 +15,7 @@ function includedNumbers(a, b, array){
 
 const numArray = [12,34,14,54,32,43,15,98,22,34,11]
 const a = 5
-const b = 20
+const b = 8
 
 console.log(includedNumbers(a,b,numArray));
 
